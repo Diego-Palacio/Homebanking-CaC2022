4d439a4551eca6d3e275c8300180097ee6f467c8
Your Name <you@example.com>
2022-12-29 22:53:49 -0300

    se agrega anotaciones para convertir clase account como entidad

--- src/main/java/models/Account.java
+++ src/main/java/models/Account.java
@@ -1,17 +1,29 @@
 package models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import utils.AccountType;
+//import utils.AccountType;
 
 @Data
+@AllArgsConstructor
+@Entity
+@Table(name = "accounts")
 public class Account {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long id;
 	public String name;
 	public Double ammount;
 	public String cbu;
 	
-	public AccountType type;
+	//public AccountType type;
 	
 	public Account() {}
 	

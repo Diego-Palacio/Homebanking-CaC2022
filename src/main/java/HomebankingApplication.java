636cd951c881ffecf221c098272544579ca2681d
Diego palacio <65735159+Diego-Palacio@users.noreply.github.com>
2023-01-06 00:07:51 -0300

    Create HomebankingApplication.java

---
+++ src/main/java/HomebankingApplication.java
@@ -0,0 +1,15 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@SpringBootApplication
+public class HomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+}
